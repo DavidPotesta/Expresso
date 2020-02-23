4ec5aff19944cc6713910d3d0ba08f42fbe74559
david <davidpotesta@gmail.com>
2020-02-23 07:24:40 -0600

    added resources and added new tabbed page

--- Expresso/App.xaml.cs
+++ Expresso/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Expresso.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,8 @@ namespace Expresso
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
+            MainPage = new HomePage();
         }
 
         protected override void OnStart()
